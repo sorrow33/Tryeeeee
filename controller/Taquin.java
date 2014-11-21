1faaa9a1cdeea0a4696e02510a5958d4e3a536c6
sorrow33 <william.sireix@gmail.com>
2014-11-21 15:26:41 +0100

    correcion du main

--- controller/Taquin.java
+++ controller/Taquin.java
@@ -3,13 +3,12 @@ package com.controller;
         import com.view.TaquinGUI;
 
         import javax.swing.*;
-        import java.awt.*;
 
 public class Taquin {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Taquin");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setContentPane(new TaquinGUI());
         window.pack();
         window.setVisible(true);
