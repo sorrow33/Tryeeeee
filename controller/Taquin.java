86e82dc5d45f2e38c841fd1a33c7e5423b6f227b
sorrow33 <william.sireix@gmail.com>
2014-11-21 15:25:45 +0100

    false

--- controller/Taquin.java
+++ controller/Taquin.java
@@ -8,19 +8,12 @@ package com.controller;
 public class Taquin {
 
     public static void main(String[] args) {
-
-        JPanel chrono = new JPanel();
-        chrono.add(new util.Chrono(120));
-
-        JFrame game = new JFrame("Taquin");
-        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.getContentPane().setLayout(new GridLayout(1,2));
-        //game.getContentPane().add(chrono);
-        game.getContentPane().add(new TaquinGUI());
-        game.setSize(400,600);
-        game.setResizable(true);
-        game.setVisible(true);
-
+        JFrame window = new JFrame("Taquin");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new TaquinGUI());
+        window.pack();
+        window.setVisible(true);
+        window.setResizable(false);
     }
 }
 
