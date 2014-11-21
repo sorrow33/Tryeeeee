fc7ab5ec3936d23c59757483c987ba2134438893
m.renaudon@gmail.com <Maxime@renaudon.1994>
2014-11-21 09:43:59 +0100

    Tentative de mettre dans une même fenêtre le compte à rebours avec le taquin mais echec (voir code commenté dans le controller

--- controller/Taquin.java
+++ controller/Taquin.java
@@ -11,14 +11,18 @@ public class Taquin {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(new TaquinGUI());
         window.pack();
-        window.setVisible(true);
         window.setResizable(true);
+        window.setVisible(true);
 
+        /*
         JFrame f = new JFrame("Test du compte à rebours");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(new TaquinGUI());
         f.add(new util.Chrono(60));
         f.pack();
+        f.setResizable(true);
         f.setVisible(true);
+        */
     }
 }
 
